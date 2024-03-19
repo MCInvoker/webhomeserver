c152e69e1787b04eaae480545e5af44e1ea1464a
陈首潮 <chenshouchao@chenshouchaos-iMac.local>
2024-03-19 15:07:46 +0800

    nothing

--- config/config.default.js
+++ config/config.default.js
@@ -7,9 +7,9 @@
  */
 module.exports = appInfo => {
     /**
-       * built-in config
-       * @type {Egg.EggAppConfig}
-       **/
+         * built-in config
+         * @type {Egg.EggAppConfig}
+         **/
     const config = exports = {};
 
     // use for cookie sign key, should change to your own and keep security
@@ -40,7 +40,7 @@ module.exports = appInfo => {
     // };
     config.sequelize = {
         dialect: "mysql", // 数据库类型，这里假设使用 MySQL
-        host: "47.97.212.228",
+        host: "localhost",
         port: 3306,
         database: "webhome", // 替换为实际的数据库名
         username: "root", // 替换为实际的数据库用户名
