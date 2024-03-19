39a934f90739352605f7b734c9afb4bac5cf4713
陈首潮 <chenshouchao@chenshouchaos-iMac.local>
2024-03-19 14:56:47 +0800

    nothing

--- config/config.default.js
+++ config/config.default.js
@@ -1,51 +1,51 @@
 /* eslint valid-jsdoc: "off" */
 
-'use strict';
+"use strict";
 
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
 module.exports = appInfo => {
     /**
-   * built-in config
-   * @type {Egg.EggAppConfig}
-   **/
+       * built-in config
+       * @type {Egg.EggAppConfig}
+       **/
     const config = exports = {};
 
     // use for cookie sign key, should change to your own and keep security
-    config.keys = appInfo.name + '_759302142';
+    config.keys = appInfo.name + "_759302142";
 
     // add your middleware config here
-    config.middleware = ['user'];
+    config.middleware = ["user"];
 
     config.security = {
         csrf: {
             enable: false,
             ignoreJSON: true,
         },
-        domainWhiteList: ['http://localhost:3000'],
+        domainWhiteList: ["http://localhost:3000"],
     };
     config.cors = {
-        origin: '*',
-        allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH',
+        origin: "*",
+        allowMethods: "GET,HEAD,PUT,POST,DELETE,PATCH",
     };
 
-    config.sequelize = {
-        dialect: 'mysql', // 数据库类型，这里假设使用 MySQL
-        host: 'localhost',
-        port: 3306,
-        database: 'webhome', // 替换为实际的数据库名
-        username: 'root', // 替换为实际的数据库用户名
-        password: 'qj759302142', // 替换为实际的数据库密码
-    };
     // config.sequelize = {
-    //     dialect: 'mysql', // 数据库类型，这里假设使用 MySQL
-    //     host: '47.97.212.228',
+    //     dialect: "mysql", // 数据库类型，这里假设使用 MySQL
+    //     host: "localhost",
     //     port: 3306,
-    //     database: 'webhome', // 替换为实际的数据库名
-    //     username: 'root', // 替换为实际的数据库用户名
-    //     password: '!Qj759302142', // 替换为实际的数据库密码
+    //     database: "webhome", // 替换为实际的数据库名
+    //     username: "root", // 替换为实际的数据库用户名
+    //     password: "qj759302142", // 替换为实际的数据库密码
     // };
+    config.sequelize = {
+        dialect: "mysql", // 数据库类型，这里假设使用 MySQL
+        host: "47.97.212.228",
+        port: 3306,
+        database: "webhome", // 替换为实际的数据库名
+        username: "root", // 替换为实际的数据库用户名
+        password: "!Qj759302142", // 替换为实际的数据库密码
+    };
 
     // config.redis = {
     //     client: {
@@ -57,11 +57,11 @@ module.exports = appInfo => {
     // };
 
     config.jwt = {
-        secret: 'invoker7777777'
+        secret: "invoker7777777",
     };
     config.bcrypt = {
-        saltRounds: 10 // default 10
-    }
+        saltRounds: 10, // default 10
+    };
 
     // add your user config here
     const userConfig = {
